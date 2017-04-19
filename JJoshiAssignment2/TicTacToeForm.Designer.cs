b1d863f5a6fa0bc73e5fc89035ea9d62d911026a
janki joshi <janki joshi>
2017-04-18 20:34:18 -0400

    Initial Commit

---
+++ JJoshiAssignment2/TicTacToeForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace JJoshiAssignment2
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pB9 = new System.Windows.Forms.PictureBox();
+            this.pB8 = new System.Windows.Forms.PictureBox();
+            this.pB7 = new System.Windows.Forms.PictureBox();
+            this.pB6 = new System.Windows.Forms.PictureBox();
+            this.pB5 = new System.Windows.Forms.PictureBox();
+            this.pB4 = new System.Windows.Forms.PictureBox();
+            this.pB3 = new System.Windows.Forms.PictureBox();
+            this.pB2 = new System.Windows.Forms.PictureBox();
+            this.pB1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pB9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pB9
+            // 
+            this.pB9.BackColor = System.Drawing.Color.White;
+            this.pB9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pB9.Location = new System.Drawing.Point(305, 305);
+            this.pB9.Name = "pB9";
+            this.pB9.Size = new System.Drawing.Size(140, 140);
+            this.pB9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pB9.TabIndex = 8;
+            this.pB9.TabStop = false;
+            this.pB9.Click += new System.EventHandler(this.pB_Click);
+            // 
+            // pB8
+            // 
+            this.pB8.BackColor = System.Drawing.Color.White;
+            this.pB8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pB8.Location = new System.Drawing.Point(159, 305);
+            this.pB8.Name = "pB8";
+            this.pB8.Size = new System.Drawing.Size(140, 140);
+            this.pB8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pB8.TabIndex = 7;
+            this.pB8.TabStop = false;
+            this.pB8.Click += new System.EventHandler(this.pB_Click);
+            // 
+            // pB7
+            // 
+            this.pB7.BackColor = System.Drawing.Color.White;
+            this.pB7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pB7.Location = new System.Drawing.Point(13, 305);
+            this.pB7.Name = "pB7";
+            this.pB7.Size = new System.Drawing.Size(140, 140);
+            this.pB7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pB7.TabIndex = 6;
+            this.pB7.TabStop = false;
+            this.pB7.Click += new System.EventHandler(this.pB_Click);
+            // 
+            // pB6
+            // 
+            this.pB6.BackColor = System.Drawing.Color.White;
+            this.pB6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pB6.Location = new System.Drawing.Point(305, 159);
+            this.pB6.Name = "pB6";
+            this.pB6.Size = new System.Drawing.Size(140, 140);
+            this.pB6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pB6.TabIndex = 5;
+            this.pB6.TabStop = false;
+            this.pB6.Click += new System.EventHandler(this.pB_Click);
+            // 
+            // pB5
+            // 
+            this.pB5.BackColor = System.Drawing.Color.White;
+            this.pB5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pB5.Location = new System.Drawing.Point(159, 159);
+            this.pB5.Name = "pB5";
+            this.pB5.Size = new System.Drawing.Size(140, 140);
+            this.pB5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pB5.TabIndex = 4;
+            this.pB5.TabStop = false;
+            this.pB5.Click += new System.EventHandler(this.pB_Click);
+            // 
+            // pB4
+            // 
+            this.pB4.BackColor = System.Drawing.Color.White;
+            this.pB4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pB4.Location = new System.Drawing.Point(13, 159);
+            this.pB4.Name = "pB4";
+            this.pB4.Size = new System.Drawing.Size(140, 140);
+            this.pB4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pB4.TabIndex = 3;
+            this.pB4.TabStop = false;
+            this.pB4.Click += new System.EventHandler(this.pB_Click);
+            // 
+            // pB3
+            // 
+            this.pB3.BackColor = System.Drawing.Color.White;
+            this.pB3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pB3.Location = new System.Drawing.Point(305, 13);
+            this.pB3.Name = "pB3";
+            this.pB3.Size = new System.Drawing.Size(140, 140);
+            this.pB3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pB3.TabIndex = 2;
+            this.pB3.TabStop = false;
+            this.pB3.Click += new System.EventHandler(this.pB_Click);
+            // 
+            // pB2
+            // 
+            this.pB2.BackColor = System.Drawing.Color.White;
+            this.pB2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pB2.Location = new System.Drawing.Point(159, 13);
+            this.pB2.Name = "pB2";
+            this.pB2.Size = new System.Drawing.Size(140, 140);
+            this.pB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pB2.TabIndex = 1;
+            this.pB2.TabStop = false;
+            this.pB2.Click += new System.EventHandler(this.pB_Click);
+            // 
+            // pB1
+            // 
+            this.pB1.BackColor = System.Drawing.Color.White;
+            this.pB1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pB1.Location = new System.Drawing.Point(13, 13);
+            this.pB1.Name = "pB1";
+            this.pB1.Size = new System.Drawing.Size(140, 140);
+            this.pB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pB1.TabIndex = 0;
+            this.pB1.TabStop = false;
+            this.pB1.Click += new System.EventHandler(this.pB_Click);
+            // 
+            // TicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(457, 459);
+            this.Controls.Add(this.pB9);
+            this.Controls.Add(this.pB8);
+            this.Controls.Add(this.pB7);
+            this.Controls.Add(this.pB6);
+            this.Controls.Add(this.pB5);
+            this.Controls.Add(this.pB4);
+            this.Controls.Add(this.pB3);
+            this.Controls.Add(this.pB2);
+            this.Controls.Add(this.pB1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TicTacToeForm";
+            this.Text = "Tic Tac Toe";
+            ((System.ComponentModel.ISupportInitialize)(this.pB9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pB1;
+        private System.Windows.Forms.PictureBox pB2;
+        private System.Windows.Forms.PictureBox pB3;
+        private System.Windows.Forms.PictureBox pB4;
+        private System.Windows.Forms.PictureBox pB5;
+        private System.Windows.Forms.PictureBox pB6;
+        private System.Windows.Forms.PictureBox pB7;
+        private System.Windows.Forms.PictureBox pB8;
+        private System.Windows.Forms.PictureBox pB9;
+    }
+}
+
---
+++ JJoshiAssignment2/TicTacToeForm.cs
@@ -0,0 +1,298 @@
+﻿
+/* 
+ * TicTacToeForm.cs
+ * Assignment 2
+ * Revision History,
+ *      Janki Joshi, 2016.10.03: designed
+ *      Janki Joshi, 2016.10.04: design [modified]
+ *      Janki Joshi, 2016.10.06: created
+ *      Janki Joshi, 2016.10.08: created[continued]
+ *      Janki Joshi, 2016.10.09: design [modified], added comments, debugged                               
+ *                               
+ */
+
+using System;
+using System.Windows.Forms;
+
+
+namespace JJoshiAssignment2
+{
+    /// <summary>
+    /// Tic Tac Toe form to set the images to pictureboxes and calculate and show the winner
+    /// </summary>
+    public partial class TicTacToeForm : Form
+    {
+        // to change the player and images from x to o and 
+        bool flag = true;
+
+        // storing the images into a bitmap
+        System.Drawing.Bitmap b = (Properties.Resources.x);
+        System.Drawing.Bitmap c = (Properties.Resources.o);
+
+        // keeps track of the winner
+        string winner;
+
+        // constant variable that has th totl number of pictureBoxes used in the form
+        const int totalPictureBoxes = 9;
+
+        // array of pictureBox
+        PictureBox[] pictureBoxArray;
+
+        /// <summary>
+        /// constructor that initializes the components of the form
+        /// </summary>
+        public TicTacToeForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// A pictureBox Click event that declares the winner and restart the game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pB_Click(object sender, System.EventArgs e)
+        {
+            pictureBoxArray = new PictureBox[totalPictureBoxes] { pB1, pB2, pB3, pB4, pB5, pB6, pB7, pB8, pB9 };
+            b.Tag = "helloX";
+            c.Tag = "helloY";
+
+            try
+            {
+                // Calling a method that initializes the image and calculates the winner
+                gameStart(sender);
+
+                if (winner == "X")
+                {
+                    MessageBox.Show(" X Wins! ", "RESULT", MessageBoxButtons.OK);
+                    for (int k = 0; k < totalPictureBoxes; k++)
+                    {
+                        if (pictureBoxArray[k] != null)
+                        {
+                            pictureBoxArray[k].Image = null;
+                            pictureBoxArray[k].Enabled = true;
+                            flag = true;
+                        }
+                    }
+                    winner = "";
+                    return;
+                }
+
+                if (winner == "O")
+                {
+                    MessageBox.Show(" O Wins! ", "RESULT", MessageBoxButtons.OK);
+                    for (int k = 0; k < totalPictureBoxes; k++)
+                    {
+                        if (pictureBoxArray[k] != null)
+                        {
+                            pictureBoxArray[k].Image = null;
+                            pictureBoxArray[k].Enabled = true;
+                            flag = true;
+                        }
+                    }
+                    winner = "";
+                    return;
+                }
+
+                int a = 0;
+                for (int j = 0; j < totalPictureBoxes; j++)
+                {
+                    if (pictureBoxArray[j].Image != null)
+                    {
+                        a++;
+                    }
+                }
+                if (a == 9)
+                {
+                    MessageBox.Show(" Stalemate! ", "RESULT", MessageBoxButtons.OK);
+                    for (int k = 0; k < totalPictureBoxes; k++)
+                    {
+                        if (pictureBoxArray[k] != null)
+                        {
+                            pictureBoxArray[k].Image = null;
+                            pictureBoxArray[k].Enabled = true;
+                            flag = true;
+                        }
+                    }
+                    winner = "";
+                    return;
+                }
+            }
+
+            catch (Exception e1)
+            {
+                e1.Message.ToString();
+            }
+        }
+
+
+        /// <summary>
+        /// methos that calculates the winner
+        /// </summary>
+        /// <param name="sender">this is an picturebox object send</param>
+
+        public void gameStart(object sender)
+        {
+
+            if (sender is PictureBox)
+            {
+                PictureBox pictureBoxReference = (PictureBox)sender;
+
+                for (int i = 0; i < totalPictureBoxes; i++)
+                {
+                    if (pictureBoxReference == pictureBoxArray[i])
+                    {
+                        if (flag == true)
+                        {
+                            pictureBoxArray[i].Image = b;
+                            pictureBoxArray[i].Image.Tag = b.Tag;
+                            flag = false;
+                            pictureBoxArray[i].Enabled = false;
+                            break;
+                        }
+                        else
+                        {
+                            pictureBoxArray[i].Image = c;
+                            pictureBoxArray[i].Image.Tag = c.Tag;
+                            flag = true;
+                            pictureBoxArray[i].Enabled = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (pictureBoxArray[0].Image != null && pictureBoxArray[1].Image != null && pictureBoxArray[2].Image != null)
+                {
+                    if (pictureBoxArray[0].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[1].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[2].Image.Tag.ToString() == b.Tag.ToString())
+                    {
+                        winner = "X";
+                    }
+                    if (pictureBoxArray[0].Image.Tag.ToString() == c.Tag.ToString()
+                        && (pictureBoxArray[1].Image.Tag.ToString()) == c.Tag.ToString()
+                        && (pictureBoxArray[2].Image.Tag.ToString() == c.Tag.ToString()))
+                    {
+                        winner = "O";
+                    }
+                }
+
+                if (pictureBoxArray[3].Image != null && pictureBoxArray[4].Image != null && pictureBoxArray[5].Image != null)
+                {
+                    if (pictureBoxArray[3].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[4].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[5].Image.Tag.ToString() == b.Tag.ToString())
+                    {
+                        winner = "X";
+                    }
+                    if (pictureBoxArray[3].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[4].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[5].Image.Tag.ToString() == c.Tag.ToString())
+                    {
+                        winner = "O";
+                    }
+                }
+
+                if (pictureBoxArray[6].Image != null && pictureBoxArray[7].Image != null && pictureBoxArray[8].Image != null)
+                {
+                    if (pictureBoxArray[6].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[7].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[8].Image.Tag.ToString() == b.Tag.ToString())
+                    {
+                        winner = "X";
+                    }
+                    if (pictureBoxArray[6].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[7].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[8].Image.Tag.ToString() == c.Tag.ToString())
+                    {
+                        winner = "O";
+                    }
+                }
+
+                if (pictureBoxArray[0].Image != null && pictureBoxArray[3].Image != null && pictureBoxArray[6].Image != null)
+                {
+                    if (pictureBoxArray[0].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[3].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[6].Image.Tag.ToString() == b.Tag.ToString())
+                    {
+                        winner = "X";
+                    }
+                    if (pictureBoxArray[0].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[3].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[6].Image.Tag.ToString() == c.Tag.ToString())
+                    {
+                        winner = "O";
+                    }
+                }
+
+                if (pictureBoxArray[1].Image != null && pictureBoxArray[4].Image != null && pictureBoxArray[7].Image != null)
+                {
+                    if (pictureBoxArray[1].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[4].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[7].Image.Tag.ToString() == b.Tag.ToString())
+                    {
+                        winner = "X";
+                    }
+                    if (pictureBoxArray[1].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[4].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[7].Image.Tag.ToString() == c.Tag.ToString())
+                    {
+                        winner = "O";
+                    }
+                }
+                if (pictureBoxArray[2].Image != null && pictureBoxArray[5].Image != null && pictureBoxArray[8].Image != null)
+                {
+                    if (pictureBoxArray[2].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[5].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[8].Image.Tag.ToString() == b.Tag.ToString())
+                    {
+                        winner = "X";
+                    }
+                    if (pictureBoxArray[2].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[5].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[8].Image.Tag.ToString() == c.Tag.ToString())
+                    {
+                        winner = "O";
+                    }
+                }
+
+
+                if (pictureBoxArray[2].Image != null && pictureBoxArray[4].Image != null && pictureBoxArray[6].Image != null)
+                {
+                    if (pictureBoxArray[2].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[4].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[6].Image.Tag.ToString() == b.Tag.ToString())
+                    {
+                        winner = "X";
+                    }
+                    if (pictureBoxArray[2].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[4].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[6].Image.Tag.ToString() == c.Tag.ToString())
+                    {
+                        winner = "O";
+                    }
+                }
+                if (pictureBoxArray[0].Image != null && pictureBoxArray[4].Image != null && pictureBoxArray[8].Image != null)
+                {
+                    if (pictureBoxArray[0].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[4].Image.Tag.ToString() == b.Tag.ToString()
+                        && pictureBoxArray[8].Image.Tag.ToString() == b.Tag.ToString())
+                    {
+                        winner = "X";
+                    }
+
+                    if (pictureBoxArray[0].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[4].Image.Tag.ToString() == c.Tag.ToString()
+                        && pictureBoxArray[8].Image.Tag.ToString() == c.Tag.ToString())
+                    {
+                        winner = "O";
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("This is not a pictureBox");
+            }
+        }
+    }
+}
